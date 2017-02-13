7deff1d8a766f3a8295a63f14baaa273f7c9d9a7
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-02-13 14:11:03 -0500

    Remaining cleanup.

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -7,22 +7,6 @@ import { actions } from '../actions';
 import { FrameworkButtonGroup } from './FrameworkButtonGroup';
 import { ProfileTable } from './ProfileTable';
 
-function results({ includeLegacy, selections }: State) {
-    if (numSelectedGroups(includeLegacy, selections) < 2)
-        return <div>Select frameworks from at least two groups to show the target PCLs.</div>;
-    const frameworks = selectedFrameworks(includeLegacy, selections);
-    const fullResult = findAllPcls(frameworks);
-    const result = removeSubsetPcls(fullResult);
-    return (
-        <div>
-            <div>You should support these PCL targets:</div>
-            <ProfileTable profiles={result}/>
-            <div>Your library will be compatible with these PCL profiles:</div>
-            <ProfileTable profiles={fullResult}/>
-        </div>
-    );
-}
-
 export function Main({ includeLegacy, selections }: State) {
     const frameworks = selectedFrameworks(includeLegacy, selections);
     const fullResult = findAllPcls(frameworks);
@@ -30,17 +14,19 @@ export function Main({ includeLegacy, selections }: State) {
 
     return (
         <div>
-            <Checkbox checked={includeLegacy} onChange={() => actions.setIncludeLegacy(!includeLegacy)}>Include legacy Frameworks</Checkbox>
+            <Checkbox checked={includeLegacy} onChange={() => actions.setIncludeLegacy(!includeLegacy)}>Include legacy frameworks ("legacy" means "not supported by VS2015")</Checkbox>
             <p>
                 <div>{getGroups(includeLegacy).map(x => <div key={x.key} className="scleft"><FrameworkButtonGroup group={x} selections={selections}/></div>)}</div>
                 <div className="scclear"/>
             </p>
-            <div>
-                <div>You should support at least these PCL targets:</div>
-                <ProfileTable profiles={result}/>
-                <div>Your library will be compatible with these PCL profiles:</div>
-                <ProfileTable profiles={fullResult}/>
-            </div>
+            {numSelectedGroups(includeLegacy, selections) < 2 ? <div>Select frameworks from at least two groups to show the target PCLs.</div> :
+                <div>
+                    <div>You should support these PCL targets:</div>
+                    <ProfileTable profiles={result}/>
+                    <div>Your library will be compatible with these PCL profiles:</div>
+                    <ProfileTable profiles={fullResult}/>
+                </div>
+            }
         </div>
     );
 }
